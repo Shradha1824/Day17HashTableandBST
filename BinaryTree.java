2ee07091a4338a3a9275e236aaa74b3ad7ec61f0
Shradha1824 <shradhabj1824@gmail.com>
2021-06-07 18:25:41 -1200

    Added file to Search Key

--- BinaryTree.java
+++ BinaryTree.java
@@ -16,76 +16,91 @@ private TreeNode root;//instance variable
 			this.data = data;
 		}
 	}	
-		public void inOrder(TreeNode root) {
-			if(root == null) {
-				return;
-			}
-
-			inOrder(root.left);
-			System.out.print(root.data + " ");
-			inOrder(root.right);
+	private TreeNode root;
+	
+	private class TreeNode {
+		private int data; // Generic type
+		private TreeNode left;
+		private TreeNode right;
+		
+		public TreeNode(int data) {
+			this.data = data;
 		}
-
-		public void inOrder() {
-			if(root == null) {
-				return;
-			}
-
-			Stack<TreeNode> stack = new Stack<>();
-			TreeNode temp = root;
-
-			while(!stack.isEmpty() || temp != null) {
-				if(temp != null) {
-					stack.push(temp);
-					temp = temp.left;
-				} else {
-					temp = stack.pop();
-					System.out.print(temp.data + " ");
-					temp = temp.right;
-				}
-			}
-		}	
+	}
+	
+	public void insert(int value) {
+		root = insert(root, value);
+	}
+	
+	public TreeNode insert(TreeNode root, int value) {
+		if(root == null) {
+			root = new TreeNode(value);
+			return root;
+		}
 		
-	public void createBinaryTree() {
-		TreeNode first = new TreeNode(56);
-		TreeNode second = new TreeNode(30);
-		TreeNode third = new TreeNode(70);
-		TreeNode fourth = new TreeNode(22);
-		TreeNode fifth = new TreeNode(40);
-		TreeNode sixth = new TreeNode(60);
-		TreeNode seventh = new TreeNode(95);
-		TreeNode eight = new TreeNode(11);
-		TreeNode nine = new TreeNode(65);
-		TreeNode ten = new TreeNode(3);
-		TreeNode eleven = new TreeNode(16);
-		TreeNode twelve = new TreeNode(63);
-		TreeNode thirteen = new TreeNode(67);
+		if(value < root.data) {
+			root.left = insert(root.left, value);
+		} else {
+			root.right = insert(root.right, value);
+		}
+		return root;
+	}
+	
+	public void inOrder() {
+		inOrder(root);
+	}
+	
+	public void inOrder(TreeNode root) {
+		if(root == null) {
+			return;
+		}
+		inOrder(root.left);
+		System.out.print(root.data + " ");
+		inOrder(root.right);
+	}
+	
+	public TreeNode search(int key) {
+		return search(root, key);
+	}
+	
+	public TreeNode search(TreeNode root, int key) {
+		if(root == null || root.data == key) { // base case 
+			return root;
+		}
 		
-		root = first;//root ---> first
-		first.left = second;
-		first.right = third;//second ---> first ---> third
-		second.left = fourth;
-		second.right = fifth;
-		third.left = sixth;
-		third.right = seventh;
-		fourth.left = eight;
-		fourth.right = null;
-		fifth.left = null;
-		fifth.right = null;
-		sixth.left = null;
-		sixth.right = nine;
-		seventh.left = null;
-		seventh.right = null;
-		eight.left = ten;
-		eight.right = eleven;
-		nine.left = twelve;
-		nine.right = thirteen;
-	}	
-		
-	public static void main(String[] args) {
-		BinaryTree bt = new BinaryTree();
-		bt.createBinaryTree();
-		bt.inOrder();
+		if(key < root.data) {
+			return search(root.left, key);
+		} else {
+			return search(root.right, key);
+		}
 		
 	}
+
+	public static void main(String[] args) {
+		BinaryTree bst = new BinaryTree();
+		bst.insert(56);
+		bst.insert(30);
+		bst.insert(70);
+		bst.insert(22);
+		bst.insert(40);
+		bst.insert(60);
+		bst.insert(95);
+		bst.insert(11);
+		bst.insert(65);
+		bst.insert(3);
+		bst.insert(16);
+		bst.insert(63);
+		bst.insert(67);
+		
+		bst.inOrder();
+				
+		System.out.println();
+		
+		if(null != bst.search(63)) {
+			System.out.println("Key found !!!");
+		} else {
+			System.out.println("Key not found !!!");
+		}
+	}
+
 }
